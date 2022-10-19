48cac14aaa8b81be6ff499636532e6a7944d6c3f
snoopinou30 <jules.snoopi@live.fr>
2022-10-19 10:35:27 +0200

    Début du .mli compil avec env et autres Ajout du type unit dans value

--- Projet/src/compil.mli
+++ Projet/src/compil.mli
@@ -1,1 +1,10 @@
-val processProgram : Ast.program CodeMap.Span.located -> unit
+open CodeMap
+open Ast
+
+type valueEnv = string * value list
+type functionEnv = string * (value list) * string list (* nom de la fct, les arguments en value, le label *)
+type environment = valueEnv * functionEnv
+
+val processProgram : program -> unit
+val eval : expression Span.located -> environment -> value
+val createFunctionLabel : string -> (value list) -> environment -> functionEnv (* Met à jour l'environnement de fonction *)
